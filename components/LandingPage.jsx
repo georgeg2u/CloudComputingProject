2ab7a64c6fe7611549fb7b11a70c8ef8e078699c
georgeg2u <george.gidoiu@gmail.com>
2024-05-10 13:31:02 +0300

    fixed npm run build bug

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 
 const LandingPage = () => {
@@ -6,13 +5,9 @@ const LandingPage = () => {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-8">Welcome to my Todo App</h1>
       <p className="text-lg text-gray-700 mb-12">
-        The best way to manage your todo's!
+        The best way to manage your todos!
       </p>
-      <img
-        src="/unnamed.png"
-        alt="Todo Image"
-        className="w-64"
-      />
+      <img src="/unnamed.png" alt="Todo Image" className="w-64" />
     </div>
   );
 };
